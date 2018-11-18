32329086774321898ed69b6d6e9dca3a358be233
Dasol Baek <schgana@hotmail.com>
2018-11-18 16:51:36 +1300

    changed text field design to material-ui

--- client/components/DailyBox.jsx
+++ client/components/DailyBox.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-// import {Button} from 'semantic-ui-react'
-// import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import {Link} from 'react-router-dom'
 
 import * as api from '../api'
+
+const styles = {
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+}
 
 export default class DailyBox extends React.Component {
   constructor (props) {
--- client/components/Normal.jsx
+++ client/components/Normal.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import {Link} from 'react-router-dom'
 
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField'
+
 import * as api from '../api'
 
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+})
 
 export default class Normal extends React.Component {
   state = {
@@ -11,10 +34,15 @@ export default class Normal extends React.Component {
     content:""
   }
 
-  changeHandler = e => {
+  changeHandler = name => event => {
     this.setState({
-      [e.target.name]: e.target.value
+      [name]: event.target.value,
     })
+  
+  // e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   })
   }
 
   addLog() {
@@ -23,12 +51,36 @@ export default class Normal extends React.Component {
   }
 
   render () {
+
     return (
       <React.Fragment>
-        <form>          
+        <form noValidate autoComplete="off">          
           <br /><br />
           <div className='writeBox'>
-          <input 
+          <TextField
+          id="standatd-name"
+          label="Keyword"
+          helperText="Keyword will be shown in the main page daily log block"
+          value={this.state.keyword}
+          onChange={this.changeHandler('keyword')}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-multiline-static-full-width"
+          label="Content"
+          //style={{ margin: 8 }}
+          placeholder="How was your day?"
+          value={this.state.content}
+          multiline
+          rows="6"
+          fullWidth
+          onChange={this.changeHandler('content')}
+          margin="normal"
+          variant="outlined"
+        />
+
+          {/* <input 
           className='input keyword' 
           type='text' 
           onChange={this.changeHandler} 
@@ -44,9 +96,10 @@ export default class Normal extends React.Component {
           value={this.state.content} 
           name='content' 
           placeholder='How was your day?' 
-          />
+          /> */}
           <br /><br />
           </div>
+
           <div className='submitBtn'>
             <Button 
             className='btn' 
